cc0071e8b298ddb07db1d65e8a666c148e562b3a
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-09-24 21:45:43 +0530

    creating react app
    First in command prompt (in vscprojects where we want our react apo) type :-  create-react-app app-name
    Then it will create react app then go to the app folder and then type:- npm start
    
    Note: This is for creating single page react app
    And start coding of js from /src/app.js file and it';s css from /src/app.css

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="box1">
+        <div id="quotesP"></div>
+        <div id="authorP">aytu</div>
+        <div id="buttons">
+          <button id="facebookB">Facebook</button>
+          <button id="nextB">Next</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
